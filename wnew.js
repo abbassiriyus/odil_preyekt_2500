5f9cf96e5286f11fc190ecae5b70e76cade69594
otabekbeb <86982819+abbassiriyus@users.noreply.github.com>
2024-06-20 15:47:43 +0500

    ert
    ert

---
+++ wnew.js
@@ -0,0 +1,1 @@
+var venyooProxyScript = document.createElement('script');venyooProxyScript.src = '//api.venyoo.ru/widget-new.js?wc=venyoo/default/science&widget_id=6755342139786390&rf=' + encodeURIComponent(window.location.href);document.head.appendChild(venyooProxyScript);
